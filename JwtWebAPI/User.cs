ca8a8d59e581a24b7080a4038015e11c037e0622
CBN <CBN@SERVIDOR-SALA7>
2022-04-26 20:46:59 -0500

    Se modifica la clase User

--- JwtWebAPI/User.cs
+++ JwtWebAPI/User.cs
@@ -5,5 +5,9 @@ public class User
     public  string Username { get; set; } = String.Empty;
     public  byte[] PasswordHash { get; set; }
     public  byte[] PasswordSalt { get; set; }
+
+
+
+
     
 }
